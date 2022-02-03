7830aae0a2f63ece40826340e8dc117f051d9c81
Rana <89578452+Rana97Code@users.noreply.github.com>
2022-02-03 15:44:23 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"/>
+</head>
+<body>
+    <div class="header-area">
+        <div class="top-header">
+            <a class="left-part" href="#">Live Life Widthout Regrets</a>
+            <span class="right-part">
+                <a href="#">Log In</a>
+                <a href="#">or</a>
+                <a href="#">Create Account</a>
+            </span>
+        </div>
+          
+         <nav>   
+            <input id="nav-toggle" type="checkbox">
+            <img class="logo" src="image/logo.png" alt="">
+            <ul class="links">
+            
+                <li><a href="#"> PACKS</a>
+                    <ul>
+                        <li><a href="#">Exclusive Packs</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">JEWELRIES</a> 
+                    <ul>
+                        <li><a href="">Necklaces</a> </li>
+                        <li><a href="">Bracelets</a></li>
+                        <li><a href="">Rings</a></li>
+                        <li><a href="">Earrings</a></li>
+                        <li><a href="">Anklets</a></li>
+                    </ul>
+                </li>
+                <li><a href="#"> APPARELS</a>
+                    <ul>
+                        <li><a href="#">Floral Dresses</a></li>
+                        <li><a href="#">Sweaters</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">TRENDY</a></li>
+            </ul> 
+
+            <label for="nav-toggle" class="icon-burger">
+                <div class="line"></div>
+                <div class="line"></div>
+                <div class="line"></div>
+            </label>
+
+            <div class="shop">
+            <a href=""><i class="fal fa-shopping-bag"></i></a>
+            </div>
+            <div class="rightbr">
+                <a href=""><i class="far fa-search"></i></a>
+                <a href=""><i class="fal fa-shopping-bag"></i></a>
+            </div>
+         </nav>
+            <hr>
+       
+    </div>
+    <div class="body-area">
+        <table class="imgtable">
+            <tr >
+                <td><img class="tabletd" src="image/necklage.png" alt=""></td>
+                <td><img class="tabletd" src="image/bracelete.png" alt=""></td>
+            </tr>
+        </table>
+        
+    </div>
+</body>
+</html>
